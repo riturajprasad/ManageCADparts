ee01ea0651bb2dcad8657a579684b9aba1b0b6bd
Rituraj Prasad <rituraj.prasad2718@gmail.com>
2025-11-15 11:39:28 +0530

    Compelete main

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -8,7 +8,7 @@
 #include "Flanges.h"
 using namespace std;
 
-void addParts(vector<CADparts*>& parts)
+void addParts(vector<shared_ptr<CADparts>>& parts)
 {
 	unsigned int ch = 0;
 	cout << "1. Pipes" << endl;
@@ -31,7 +31,7 @@ void addParts(vector<CADparts*>& parts)
 		cout << "Enter Pipes's Length: " << endl;
 		cin >> length;
 		if (length < 0) throw "Length never be negative\n";
-		parts.push_back(new Pipes(length, material, diameter));
+		parts.push_back(make_shared<Pipes>(length, material, diameter));
 		cout << "Pipes Added succesfully!" << endl;
 	}
 	else if (ch == 2)
@@ -47,7 +47,7 @@ void addParts(vector<CADparts*>& parts)
 		cout << "Enter Valves's No of Opening: " << endl;
 		cin >> opening;
 		if (opening < 0) throw "Opening never be negative\n";
-		parts.push_back(new Valves(opening, material, diameter));
+		parts.push_back(make_shared<Valves>(opening, material, diameter));
 		cout << "Valves Added succesfully!" << endl;
 	}
 	else if (ch == 3)
@@ -63,7 +63,7 @@ void addParts(vector<CADparts*>& parts)
 		cout << "Enter Elbows's Length: " << endl;
 		cin >> length;
 		if (length < 0) throw "Length never be negative\n";
-		parts.push_back(new Elbows(length, material, diameter));
+		parts.push_back(make_shared<Elbows>(length, material, diameter));
 		cout << "Elbows Added succesfully!" << endl;
 	}
 	else if (ch == 4)
@@ -78,58 +78,54 @@ void addParts(vector<CADparts*>& parts)
 		if (diameter < 0) throw "Diameter never be negative\n";
 		cout << "Enter Flanges's Quilty: " << endl;
 		cin >> quilty;
-		parts.push_back(new Flanges(quilty, material, diameter));
+		parts.push_back(make_shared<Flanges>(quilty, material, diameter));
 		cout << "Flanges Added succesfully!" << endl;
 	}
 	else cout << "Wrong number, please check the CAD parts list." << endl;
 }
-void viewParts(const vector<CADparts*> &parts)
+void viewParts(const vector<shared_ptr<CADparts>>&parts)
 {
-	for (auto it : parts)
+	for (const auto& it : parts)
 	{
-		if (it->getName() == "Pipe")
+		if (auto p = dynamic_cast<Pipes*>(it.get()))
 		{
-			Pipes* p = dynamic_cast<Pipes*>(it);
 			cout << p->getName() << " with Id number " << p->getPartsId() << endl;
-			cout << "Use " << p->getMaterial() << " Mateials" << endl;
+			cout << "Use " << p->getMaterial() << " Materials" << endl;
 			cout << "Diameter = " << p->getDiameter() << endl;
 			cout << "Length = " << p->getLength() << endl << endl;
 		}
-		if (it->getName() == "Valve")
+		else if (auto v = dynamic_cast<Valves*>(it.get()))
 		{
-			Valves* v = dynamic_cast<Valves*>(it);
 			cout << v->getName() << " with Id number " << v->getPartsId() << endl;
-			cout << "Use " << v->getMaterial() << " Mateials" << endl;
+			cout << "Use " << v->getMaterial() << " Materials" << endl;
 			cout << "Diameter = " << v->getDiameter() << endl;
 			cout << "No of Opening = " << v->getNo_ofOpening() << endl << endl;
 		}
-		if (it->getName() == "Elbow")
+		else if (auto e = dynamic_cast<Elbows*>(it.get()))
 		{
-			Elbows* p = dynamic_cast<Elbows*>(it);
-			cout << p->getName() << " with Id number " << p->getPartsId() << endl;
-			cout << "Use " << p->getMaterial() << " Mateials" << endl;
-			cout << "Diameter = " << p->getDiameter() << endl;
-			cout << "Area = " << p->getArea() << endl << endl;
+			cout << e->getName() << " with Id number " << e->getPartsId() << endl;
+			cout << "Use " << e->getMaterial() << " Materials" << endl;
+			cout << "Diameter = " << e->getDiameter() << endl;
+			cout << "Area = " << e->getArea() << endl << endl;
 		}
-		if (it->getName() == "Flange")
+		else if (auto f = dynamic_cast<Flanges*>(it.get()))
 		{
-			Flanges* p = dynamic_cast<Flanges*>(it);
-			cout << p->getName() << " with Id number " << p->getPartsId() << endl;
-			cout << "Use " << p->getMaterial() << " Mateials" << endl;
-			cout << "Diameter = " << p->getDiameter() << endl;
-			cout << "Quilty = " << p->getQuilty() << endl << endl;
+			cout << f->getName() << " with Id number " << f->getPartsId() << endl;
+			cout << "Use " << f->getMaterial() << " Materials" << endl;
+			cout << "Diameter = " << f->getDiameter() << endl;
+			cout << "Quality = " << f->getQuilty() << endl << endl;
 		}
 	}
 }
-void searchPartsbyId(const vector<CADparts*>& parts, const unsigned int &id)
+void searchPartsbyId(const vector<shared_ptr<CADparts>>& parts, const unsigned int &id)
 {
 	bool found = false;
-	for (auto it : parts)
+	for (auto &it : parts)
 	{
 		if (it->getName() == "Pipe" && it->getPartsId() == id)
 		{
 			cout << "Parts found!" << endl;
-			Pipes* p = dynamic_cast<Pipes*>(it);
+			Pipes* p = dynamic_cast<Pipes*>(it.get());
 			cout << p->getName() << " with Id number " << p->getPartsId() << endl;
 			cout << "Use " << p->getMaterial() << " Mateials" << endl;
 			cout << "Diameter = " << p->getDiameter() << endl;
@@ -139,7 +135,7 @@ void searchPartsbyId(const vector<CADparts*>& parts, const unsigned int &id)
 		if (it->getName() == "Valve" && it->getPartsId() == id)
 		{
 			cout << "Parts found!" << endl;
-			Valves* v = dynamic_cast<Valves*>(it);
+			Valves* v = dynamic_cast<Valves*>(it.get());
 			cout << v->getName() << " with Id number " << v->getPartsId() << endl;
 			cout << "Use " << v->getMaterial() << " Mateials" << endl;
 			cout << "Diameter = " << v->getDiameter() << endl;
@@ -149,7 +145,7 @@ void searchPartsbyId(const vector<CADparts*>& parts, const unsigned int &id)
 		if (it->getName() == "Elbow" && it->getPartsId() == id)
 		{
 			cout << "Parts found!" << endl;
-			Elbows* p = dynamic_cast<Elbows*>(it);
+			Elbows* p = dynamic_cast<Elbows*>(it.get());
 			cout << p->getName() << " with Id number " << p->getPartsId() << endl;
 			cout << "Use " << p->getMaterial() << " Mateials" << endl;
 			cout << "Diameter = " << p->getDiameter() << endl;
@@ -159,7 +155,7 @@ void searchPartsbyId(const vector<CADparts*>& parts, const unsigned int &id)
 		if (it->getName() == "Flange" && it->getPartsId() == id)
 		{
 			cout << "Parts found!" << endl;
-			Flanges* p = dynamic_cast<Flanges*>(it);
+			Flanges* p = dynamic_cast<Flanges*>(it.get());
 			cout << p->getName() << " with Id number " << p->getPartsId() << endl;
 			cout << "Use " << p->getMaterial() << " Mateials" << endl;
 			cout << "Diameter = " << p->getDiameter() << endl;
@@ -169,7 +165,7 @@ void searchPartsbyId(const vector<CADparts*>& parts, const unsigned int &id)
 	}
 	if (!found) cout << "Parts Not found!" << endl;
 }
-void deletePartsbyId(vector<CADparts*>& parts, const unsigned int& id)
+void deletePartsbyId(vector<shared_ptr<CADparts>>& parts, const unsigned int& id)
 {
 	bool found = false;
 	for (int i=0; i<parts.size(); i++)
@@ -177,7 +173,7 @@ void deletePartsbyId(vector<CADparts*>& parts, const unsigned int& id)
 		if (parts[i]->getPartsId() == id)
 		{
 			cout << "Parts Deleted!" << endl;
-			delete parts[i];
+			// delete parts[i].get();
 			parts.erase(parts.begin() + i);
 			found = true;
 		}
@@ -187,7 +183,7 @@ void deletePartsbyId(vector<CADparts*>& parts, const unsigned int& id)
 
 int main()
 {
-	vector<CADparts*> parts;
+	vector<shared_ptr<CADparts>> parts;
 	while (true)
 	{
 		unsigned int choice = 0;
@@ -206,7 +202,7 @@ int main()
 		if (choice == 0)
 		{
 			if (parts.size() == 0) break;
-			for (auto it : parts) delete it;
+			// for (auto &it : parts) delete it.get();
 			parts.clear();
 			break;
 		}
@@ -249,8 +245,8 @@ int main()
 				cout << "No parts avilable" << endl;
 				continue;
 			}
-			vector<CADparts*> parts2 = parts;
-			sort(parts2.begin(), parts2.end(), [](CADparts* cadp1, CADparts* cadp2) {return (cadp1->getDiameter() < cadp2->getDiameter()); });
+			vector<shared_ptr<CADparts>> parts2 = parts;
+			sort(parts2.begin(), parts2.end(), [](const shared_ptr<CADparts>& cadp1, const shared_ptr<CADparts>& cadp2) {return (cadp1->getDiameter() < cadp2->getDiameter()); });
 			cout << "All parts after sort" << endl;
 			viewParts(parts2);
 		}
